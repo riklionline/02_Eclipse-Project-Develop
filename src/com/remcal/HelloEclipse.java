1526d27a304c9edea6f4772d105c66a6ce3fa3e0
FAT-Remcal <lilianmeng1987@163.com>
2019-08-23 13:08:03 +0800

    updata eclipse code.

---
+++ src/com/remcal/HelloEclipse.java
@@ -0,0 +1,17 @@
+package com.remcal;
+
+/**
+ * 
+ * @company Foundation of Art and Technology
+ * @author Remcal
+ * @description Eclipse test
+ * Aug 2, 2019
+ */
+public class HelloEclipse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("hello eclipse!!!");//ÄãºÃ£¬Eclipse
+	}
+
+}
